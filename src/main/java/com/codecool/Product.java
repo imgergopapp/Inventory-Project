c272edd47d0ca4ecc303fd456895222a2f8a1fed
Gergő Papp <imgergopapp@gmail.com>
2019-02-24 22:55:40 +0100

    reformatted with ProductType enum

--- src/main/java/com/codecool/Product.java
+++ src/main/java/com/codecool/Product.java
@@ -3,17 +3,24 @@ package com.codecool;
 public abstract class  Product {
     private String name;
     private int price;
+    private ProductType type;
 
-    public Product (String name, int price){
+    public Product (String name, int price, ProductType type){
         this.name = name;
         this.price = price;
+        this.type = type;
     }
 
     public String getName() {
         return name;
     }
 
+    public ProductType getType() {
+        return type;
+    }
+
     public int getPrice() {
         return price;
     }
+
 }
