7dc6dfdccbb7f5829070a6922806f6006f33a7a8
Gergő Papp <imgergopapp@gmail.com>
2019-02-25 19:45:45 +0100

    implemented storeProduct abstract method

--- src/main/java/com/codecool/PersistentStore.java
+++ src/main/java/com/codecool/PersistentStore.java
@@ -1,8 +1,9 @@
 package com.codecool;
 
-public class PersistentStore extends Store{
+public class PersistentStore extends Store {
 
-    public void storeProduct (Product product){
-
+    public void storeProduct(Product product) {
+        addToStoredProducts(product);
+        System.out.println(product.getName() + " added to Stored product list.");
     }
 }
