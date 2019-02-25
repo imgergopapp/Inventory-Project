b283c92b9db041382acd9408918dbd4ad1663230
Gergő Papp <imgergopapp@gmail.com>
2019-02-25 19:48:02 +0100

    Refactored

--- src/main/java/com/codecool/Output.java
+++ src/main/java/com/codecool/Output.java
@@ -3,9 +3,9 @@ package com.codecool;
 import java.util.List;
 
 public class Output {
-    public static void printProducts(List<Product> products,String message){
+    public static void printProducts(List<Product> products, String message) {
         System.out.println("\n" + message);
-        for(Product product : products){
+        for (Product product : products) {
             System.out.println(product);
         }
     }
