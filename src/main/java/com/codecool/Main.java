a46a4e87cdf86856a7768d7ee5a88378cb77a594
Gergő Papp <imgergopapp@gmail.com>
2019-02-25 19:44:36 +0100

    Refactored

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -9,18 +9,18 @@ public class Main {
         StoreManager storeManager = new StoreManager();
 
         List<Product> products = storeManager.storageCapable.getAllProduct();
-        Output.printProducts(products,"All products ");
+        Output.printProducts(products, "All products ");
 
         int totalPrice = storeManager.getTotalProductPrice();
         System.out.println("\nThe total price of all products is " + totalPrice + ".\n");
 
         storeManager.addBookProduct("Night Shift", 25, 336);
         storeManager.addCDProduct("AM", 18, 12);
-        List<Product> storeProducts = storeManager.persistentStore.getStoredProducts() ;
+        List<Product> storeProducts = storeManager.persistentStore.getStoredProducts();
         Output.printProducts(storeProducts, "Stored products: ");
 
         products = storeManager.storageCapable.getAllProduct();
-        Output.printProducts(products,"All products: ");
+        Output.printProducts(products, "All products: ");
 
         totalPrice = storeManager.getTotalProductPrice();
         System.out.println("The total price of all products is " + totalPrice + ".");
