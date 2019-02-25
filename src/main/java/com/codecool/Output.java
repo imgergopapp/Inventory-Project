9fd132cb2e93cdf1c6119b3c9d83f56e13c68a41
Gergő Papp <imgergopapp@gmail.com>
2019-02-25 19:45:10 +0100

    added printProducts method

--- src/main/java/com/codecool/Output.java
+++ src/main/java/com/codecool/Output.java
@@ -1,4 +1,14 @@
 package com.codecool;
 
+import java.util.List;
+
 public class Output {
+    public static void printProducts(List<Product> products,String message){
+        System.out.println("\n" + message);
+        for(Product product : products){
+            System.out.println(product);
+        }
+    }
+
+
 }
