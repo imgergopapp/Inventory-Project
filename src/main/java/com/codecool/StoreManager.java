e187b18331467f399c8520698a95a3fd241ee57b
Gergő Papp <imgergopapp@gmail.com>
2019-02-25 19:47:39 +0100

    implemented StorageCapable interface &  methods

--- src/main/java/com/codecool/StoreManager.java
+++ src/main/java/com/codecool/StoreManager.java
@@ -2,47 +2,54 @@ package com.codecool;
 
 import java.util.List;
 
-public class StoreManager  {
-    private StorageCapable storageCapable;
-    private Store store;
-    private ProductType type;
+public class StoreManager {
 
-    public void addStorage(StorageCapable storage){
-        storageCapable = new StorageCapable() {
+    PersistentStore persistentStore = new PersistentStore();
 
-            @Override
-            public List<Product> getAllProduct() {
-                return null;
-            }
+    StorageCapable storageCapable = new StorageCapable() {
 
-            @Override
-            public void storeCDProduct(String name, int price, int tracks) {
-                type = ProductType.CD;
-                store.createProduct(type,name,price,tracks);
+        @Override
+        public List<Product> getAllProduct() {
+            List<Product> products = persistentStore.loadProducts();
+            return products;
+        }
 
-            }
+        @Override
+        public void storeCDProduct(String name, int price, int tracks) {
+            Product product = persistentStore.createProduct(ProductType.CD, name, price, tracks);
+            persistentStore.store(product);
+        }
 
-            @Override
-            public void storeBookProduct(String name, int price, int pages) {
-                type = ProductType.BOOK;
-                store.createProduct(type,name,price,pages);
+        @Override
+        public void storeBookProduct(String name, int price, int pages) {
+            Product product = persistentStore.createProduct(ProductType.BOOK, name, price, pages);
+            persistentStore.store(product);
+        }
+    };
 
-            }
-        };
-    }
-    public void addCDProduct(String name, int price, int tracks){
+    public void addCDProduct(String name, int price, int tracks) {
         storageCapable.storeCDProduct(name, price, tracks);
-
     }
-    public void addBookProduct(String name, int price, int pages){
+
+    public void addBookProduct(String name, int price, int pages) {
         storageCapable.storeBookProduct(name, price, pages);
     }
 
-    public String listProducts(){
-
+    public String listProducts() {
+        List<Product> products = storageCapable.getAllProduct();
+        String productList = " ";
+        for (Product product : products) {
+            productList += product.getName() + ", ";
+        }
+        return productList.substring(0, productList.length() - 2);
     }
 
-    public int getTotalProductPrice(){
-
+    public int getTotalProductPrice() {
+        List<Product> products = storageCapable.getAllProduct();
+        int sum = 0;
+        for (Product product : products) {
+            sum += product.getPrice();
+        }
+        return sum;
     }
 }
