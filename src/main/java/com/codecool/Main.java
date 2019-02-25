f05d21c43238d58036b2409c7f88cab415cb2391
Gergő Papp <imgergopapp@gmail.com>
2019-02-25 19:44:22 +0100

    added method calls

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,14 +1,28 @@
 package com.codecool;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-    /*The Main class is responsible for starting the application.
-     It create a StoreManager instance to be able to store products.*/
 
-    StoreManager storeManager = new StoreManager();
+        StoreManager storeManager = new StoreManager();
 
+        List<Product> products = storeManager.storageCapable.getAllProduct();
+        Output.printProducts(products,"All products ");
 
+        int totalPrice = storeManager.getTotalProductPrice();
+        System.out.println("\nThe total price of all products is " + totalPrice + ".\n");
 
+        storeManager.addBookProduct("Night Shift", 25, 336);
+        storeManager.addCDProduct("AM", 18, 12);
+        List<Product> storeProducts = storeManager.persistentStore.getStoredProducts() ;
+        Output.printProducts(storeProducts, "Stored products: ");
+
+        products = storeManager.storageCapable.getAllProduct();
+        Output.printProducts(products,"All products: ");
+
+        totalPrice = storeManager.getTotalProductPrice();
+        System.out.println("The total price of all products is " + totalPrice + ".");
     }
 }
